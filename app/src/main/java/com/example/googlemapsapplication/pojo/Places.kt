0e4aaf551ec83b6dddde46e2fdec059a1db8625c
Dmitry Pevnitsky <pandarec13@yandex.ru>
2021-04-28 00:58:44 +0300

    wip

--- app/src/main/java/com/example/googlemapsapplication/pojo/Places.kt
+++ app/src/main/java/com/example/googlemapsapplication/pojo/Places.kt
@@ -5,15 +5,7 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
-//data class Coords (
-//    @SerializedName("lat")
-//    @Expose
-//    var lat: Double = 0.0,
-//
-//    @SerializedName("lon")
-//    @Expose
-//    var lon: Double = 0.0
-//)
+
 
 class Coords:Serializable{
     @SerializedName("lat")
@@ -25,12 +17,7 @@ class Coords:Serializable{
     var lon: Double = 0.0
 }
 
-//data class Image (
-//    @SerializedName("image")
-//    @Expose
-//    var image: String? = null
-//
-//)
+
 
 class Image: Serializable{
     @SerializedName("image")
@@ -54,6 +41,63 @@ data class Place (
     @Expose
     var results: List<Result> = listOf()
 )
+
+
+class Result:Serializable{
+    @SerializedName("id")
+    @Expose
+    var id: Int = 0
+
+    @SerializedName("title")
+    @Expose
+    var title: String? = null
+
+    @SerializedName("address")
+    @Expose
+    var address: String? = null
+
+    @SerializedName("timetable")
+    @Expose
+    var timetable: String? = null
+
+    @SerializedName("phone")
+    @Expose
+    var phone: String? = null
+
+    @SerializedName("body_text")
+    @Expose
+    var bodyText: String? = null
+
+    @SerializedName("description")
+    @Expose
+    var description: String? = null
+
+    @SerializedName("site_url")
+    @Expose
+    var siteUrl: String? = null
+
+    @SerializedName("foreign_url")
+    @Expose
+    var foreignUrl: String? = null
+
+    @SerializedName("coords")
+    @Expose
+    var coords: Coords? = null
+
+    @SerializedName("subway")
+    @Expose
+    var subway: String? = null
+
+    @SerializedName("favorites_count")
+    @Expose
+    var favoritesCount: Int = 0
+
+    @SerializedName("images")
+    @Expose
+    var images: List<Image> = listOf()
+}
+
+//////////////////////////////////////////////////////////////////////
 //
 //data class Result (
 //    @SerializedName("id")
@@ -109,56 +153,20 @@ data class Place (
 //    var images: List<Image> = listOf()
 //):Serializable
 
-class Result:Serializable{
-    @SerializedName("id")
-    @Expose
-    var id: Int = 0
 
-    @SerializedName("title")
-    @Expose
-    var title: String? = null
+//data class Coords (
+//    @SerializedName("lat")
+//    @Expose
+//    var lat: Double = 0.0,
+//
+//    @SerializedName("lon")
+//    @Expose
+//    var lon: Double = 0.0
+//)
 
-    @SerializedName("address")
-    @Expose
-    var address: String? = null
-
-    @SerializedName("timetable")
-    @Expose
-    var timetable: String? = null
-
-    @SerializedName("phone")
-    @Expose
-    var phone: String? = null
-
-    @SerializedName("body_text")
-    @Expose
-    var bodyText: String? = null
-
-    @SerializedName("description")
-    @Expose
-    var description: String? = null
-
-    @SerializedName("site_url")
-    @Expose
-    var siteUrl: String? = null
-
-    @SerializedName("foreign_url")
-    @Expose
-    var foreignUrl: String? = null
-
-    @SerializedName("coords")
-    @Expose
-    var coords: Coords? = null
-
-    @SerializedName("subway")
-    @Expose
-    var subway: String? = null
-
-    @SerializedName("favorites_count")
-    @Expose
-    var favoritesCount: Int = 0
-
-    @SerializedName("images")
-    @Expose
-    var images: List<Image> = listOf()
-}
+//data class Image (
+//    @SerializedName("image")
+//    @Expose
+//    var image: String? = null
+//
+//)
